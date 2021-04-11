a5c21b35f39bb157de42e8d83f94fb00168dcd29
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-04-11 22:00:14 +0200

    links

--- src/js/someTask.js
+++ src/js/someTask.js
@@ -1,5 +1,16 @@
 let b = 20;
 if (true) {
-  console.log(b);
+  console.log(b); //undefined
   let b = 10;
 }
+
+//
+
+let array = [];
+array['10'] = 5;
+console.log(array.length); //11
+
+//
+// const x = 1;
+// const x = 'hello';
+// console.log(x);
