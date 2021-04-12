245698ad4ed49fd5358cc7d4b6826dd546ec9da9
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-04-12 23:41:37 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -240,7 +240,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53160" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53595" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
